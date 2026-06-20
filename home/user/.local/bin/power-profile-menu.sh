4d6d21e6c2d43f8437d7e1bd7b5f30ddd4a9f583
cyberpz <cyberpz@users.noreply.github.com>
2026-06-20 19:38:27 +0200

    Fix menu batteria: esporta DISPLAY/XAUTHORITY e path assoluto rofi

--- home/user/.local/bin/power-profile-menu.sh
+++ home/user/.local/bin/power-profile-menu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Menu rofi per scegliere il profilo energetico.
+export DISPLAY="${DISPLAY:-:0}"
+export XAUTHORITY="${XAUTHORITY:-$HOME/.Xauthority}"
 
-set -u
-
-CHOICE=$(printf "Performance\nComfort\nEco" | rofi -dmenu -p "Profilo" -theme power-profile)
+CHOICE=$(printf "Performance\nComfort\nEco" | /usr/bin/rofi -dmenu -p "Profilo" -theme power-profile)
 
 [ -z "$CHOICE" ] && exit 0
 exec /home/user/.local/bin/power-profile-apply.sh "$CHOICE"
--- home/user/.local/bin/power-profile-toggle.sh
+++ home/user/.local/bin/power-profile-toggle.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Apre il menu per scegliere il profilo energetico.
+export DISPLAY="${DISPLAY:-:0}"
+export XAUTHORITY="${XAUTHORITY:-$HOME/.Xauthority}"
+touch /tmp/power-profile-click-test
 exec /home/user/.local/bin/power-profile-menu.sh
